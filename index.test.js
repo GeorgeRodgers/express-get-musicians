48d4c6d42d364706f584c2051e8f0649022fb87a
George Rodgers <george.rodgers.chem@gmail.com>
2024-10-14 14:12:30 +0100

    Express Musicians Part 1 (+ extension) complete

--- index.test.js
+++ index.test.js
@@ -12,11 +12,14 @@ const {seedMusician} = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
-    
+    // Test written for Express Musicians Part 1
 
-
-
-
+    test(`gets the correct response`, async () => {
+        const response = await request(app).get(`/musicians`);
+        expect(response.statusCode).toBe(200); // Check status code for the response
+        const musician = await Musician.findByPk(1);
+        const responseData = JSON.parse(response.text);
+        expect(musician.dataValues.name).toEqual(responseData[0].name) // compares the data from the response to the database
+    });
     
 })
--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 
+// Code for Express Musicians Part 1
 
+app.get(`/musicians`, async (req, res) => {
+    const musicians = await Musician.findAll({});
+    res.json(musicians);
+})
 
+// Codde for Express Musicians Part 1 extension
 
+app.get(`/musicians/1`, async (req, res) => {
+    const musician1 = await Musician.findByPk(1);
+    res.json(musician1);
+})
 
-
+app.get(`/bands`, async (req, res) => {
+    const bands = await Band.findAll({});
+    res.json(bands);
+})
 
 module.exports = app;
