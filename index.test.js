207b38357ca7c91545d6e371ad8dd9b49e79a43c
George Rodgers <george.rodgers.chem@gmail.com>
2024-10-15 14:08:28 +0100

    Express Musicians Part 3 complete

--- index.test.js
+++ index.test.js
@@ -9,19 +9,26 @@ const { Musician } = require('./models/index')
 const app = require('./src/app');
 const {seedMusician} = require("./seedData");
 
+// Test written for Express Musicians Part 1
 
 describe('./musicians endpoint', () => {
-    // Write your tests here
-    // Test written for Express Musicians Part 1
-
     test(`gets the correct response`, async () => {
-        const response = await request(app).get(`/musicians`);
+        let response = await request(app).get(`/musicians`);
         expect(response.statusCode).toBe(200); // Check status code for the response
-        const musician = await Musician.findByPk(1);
-        const responseData = JSON.parse(response.text);
+        let musician = await Musician.findByPk(1);
+        let responseData = JSON.parse(response.text);
         expect(musician.dataValues.name).toEqual(responseData[0].name) // compares the data from the response to the database
     });
+});
 
-    // need to write tests for Musicians Part 2
-    
-})
+// need to write tests for Musicians Part 2
+
+describe('./musicians/:id endpoint', () => {
+    test(`gets the correct response`, async () => {
+        response = await request(app).get(`/musicians/2`);
+        expect(response.statusCode).toBe(200); // Check status code for the response
+        musician = await Musician.findByPk(2);
+        responseData = JSON.parse(response.text);
+        expect(musician.name).toEqual(responseData.name);
+    });
+});
